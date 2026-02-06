6ec030e407f8d4ab90f63c7efadda7837996e565
sudhanalight-art <sudhana.light@gmail.com>
2026-02-06 14:50:17 +0000

    fix: force resolve build errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ 確保這裡與您的 Repo 名稱完全一致，前後都要有斜線
+  // ⚠️ 這裡一定要是您的專案名稱，前後都要有斜線 /
   base: '/Household-Net-Worth-Tracker/',
+  build: {
+    // 忽略大檔案警告
+    chunkSizeWarningLimit: 1600,
+  }
 })
